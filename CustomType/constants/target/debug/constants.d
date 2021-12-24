93693d8e277a3d7a737d6c71e311130caaf38a4f
socr102 <normanburtonfree@gmail.com>
2021-12-24 15:29:18 -0800

    constants

---
+++ CustomType/constants/target/debug/constants.d
@@ -0,0 +1,1 @@
+/home/eric/Git/Rust-pro/CustomType/constants/target/debug/constants: /home/eric/Git/Rust-pro/CustomType/constants/src/main.rs
---
+++ CustomType/constants/target/debug/deps/constants-64acb60126dc353a.d
@@ -0,0 +1,5 @@
+/home/eric/Git/Rust-pro/CustomType/constants/target/debug/deps/constants-64acb60126dc353a: src/main.rs
+
+/home/eric/Git/Rust-pro/CustomType/constants/target/debug/deps/constants-64acb60126dc353a.d: src/main.rs
+
+src/main.rs:
